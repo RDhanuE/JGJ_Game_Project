70bf84109187e578865b6f3c8f77e41a96d90398
farhanhnfx <farhanhanif@mail.ugm.ac.id>
2023-04-16 20:23:44 +0700

    add player marker (dog)

--- 2D Project/Assets/Scripts/Alternate.cs
+++ 2D Project/Assets/Scripts/Alternate.cs
@@ -13,9 +13,11 @@ public class Alternate : MonoBehaviour
             if (i == 0){
                 ghosts[i].GetComponent<Movement>().enabled = true;
                 ghosts[i].GetComponent<AutomaticMovement>().enabled = false;
+                ghosts[i].transform.GetChild(0).gameObject.SetActive(true);
             } else {
                 ghosts[i].GetComponent<Movement>().enabled = false;
                 ghosts[i].GetComponent<AutomaticMovement>().enabled = true;    
+                ghosts[i].transform.GetChild(0).gameObject.SetActive(false);
             }
         }
     }
@@ -26,12 +28,14 @@ public class Alternate : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C)){
             ghosts[position].GetComponent<Movement>().enabled = false;
             ghosts[position].GetComponent<AutomaticMovement>().enabled = true;
+            ghosts[position].transform.GetChild(0).gameObject.SetActive(false);
             position++;
             if (position == ghosts.Length){
                 position = 0;
             }
             ghosts[position].GetComponent<Movement>().enabled = true;
             ghosts[position].GetComponent<AutomaticMovement>().enabled = false;
+            ghosts[position].transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 }
