6c83b42d4c2e90a78109e508fc616261aaaf0236
farhanhnfx <farhanhanif@mail.ugm.ac.id>
2023-04-16 21:52:46 +0700

    random dog, cat spawn pos

--- 2D Project/Assets/Scripts/GameManager.cs
+++ 2D Project/Assets/Scripts/GameManager.cs
@@ -8,10 +8,33 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]private GameObject gameOverScreen;
     [SerializeField]private Button retryButton;
+    [SerializeField]private List<GameObject> ghosts = new List<GameObject>();
+    [SerializeField]private GameObject cat;
     private void Start() {
         IsOver(false);
         retryButton = gameOverScreen.GetComponentInChildren<Button>();
         retryButton.onClick.AddListener(delegate { ReloadScene(); });
+        List<Vector2> ghostA_spawnPos = new List<Vector2>(3);
+        List<Vector2> ghostB_spawnPos = new List<Vector2>(3);
+        List<Vector2> cat_spawnPos = new List<Vector2>(3);
+        for (int i = 0; i < 3; i++) {
+            ghostA_spawnPos.Add(GameObject.Find($"Spawn{i+1}_a").transform.position);
+            ghostB_spawnPos.Add(GameObject.Find($"Spawn{i+1}_b").transform.position);
+            cat_spawnPos.Add(GameObject.Find($"Spawn{i+1}_c").transform.position);
+        }
+        
+        int x = Random.Range(0, 3);
+        ghosts[0].GetComponent<AutomaticMovement>().movePoint.position = ghostA_spawnPos[x];
+        ghosts[0].transform.position = ghostA_spawnPos[x];
+        Debug.Log(x);
+        x = Random.Range(0, 3);
+        Debug.Log(x);
+        ghosts[1].GetComponent<AutomaticMovement>().movePoint.position = ghostB_spawnPos[x];
+        ghosts[1].transform.position = ghostB_spawnPos[x];
+        x = Random.Range(0, 3);
+        Debug.Log(x);
+        cat.GetComponent<CatMovement>().movePoint.position = cat_spawnPos[x];
+        cat.transform.position = cat_spawnPos[x];
     }
     public void IsOver(bool isOver) {
         if (isOver) {
